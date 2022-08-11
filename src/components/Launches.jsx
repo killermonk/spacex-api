1299b968c2291a416b1ff799263908dfea94b0a7
enitial <spam@literecords.com>
2022-08-11 17:44:04 +0100

    grid created, launch badge, date and details currently showing

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/col";
 
 const Launches = props => {
+
+    const getLaunchpad = (id) => {
+        axios
+          .get("https://api.spacexdata.com/v5/launchpads/" & id)
+          .then((response) => {
+            console.log("launchpad: ", response.data);
+          })
+      }    
 
   return (
     <div>
@@ -11,6 +20,36 @@ const Launches = props => {
       ) : (
         props.launches.map(launch =>
             (
+                <div className="p-4 border rounded m-4" key={props.key}>
+                    <Row className="p-4">
+                        <Col>
+                            <h2>{launch.name}</h2><br/>
+                            <img src={launch.links.patch.small} />
+                        </Col>
+                        <Col>
+                        <dl>
+                            <dt>Date</dt>
+                            <dd>{launch.date_utc}</dd>
+                            {launch.details ? (
+                                    <>
+                                        <dt>Details</dt>
+                                        <dd>{launch.details}</dd>
+                                    </>) : 
+                                ("")}
+
+                        </dl>
+                        </Col>
+                    </Row>
+                    <Row>
+                    <details>
+                            <summary>More launch details</summary>
+                            <br/>
+                            <dl>
+                            </dl>
+                        </details>
+
+                    </Row>
+                </div>
             ))
       )
     }
